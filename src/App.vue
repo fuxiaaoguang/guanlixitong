e39bf1f541fed1c9835264ad472addad684a5e01
fuxiaaoguang <977514962@qq.com>
2019-02-16 11:04:04 +0800

    重置  login样式

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="app">
-    
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <router-view> </router-view>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name:'App'
 }
 </script>
 
-<style>
 
+<style>
+  #app {
+      height: 100%;
+  }
 </style>
