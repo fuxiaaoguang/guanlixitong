075d6aa6d12e8d771bb8ff6e88b920f24c069dd6
fuxiaoguang <977514964@qq.com>
2019-09-21 12:14:36 +0800

    xinzeng

---
+++ src/components/nihao.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page">
+     trtrtrtrtrt
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped >
+</style>
