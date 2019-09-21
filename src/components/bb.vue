601de0737021aa4d81d492d84ca9793c9ea1b3ca
fuxiaoguang <977514964@qq.com>
2019-09-21 12:27:49 +0800

    bb

---
+++ src/components/bb.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page">
+
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped >
+</style>
